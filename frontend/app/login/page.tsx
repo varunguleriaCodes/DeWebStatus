ea4be25293b471843fdc20acc31d85ae3b982666
Varun Guleria <varunguleria01@gmail.com>
2025-05-02 00:48:36 +0530

    feat: remove_unused_parts

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -9,7 +9,7 @@ import * as z from "zod";
 import { Eye, EyeOff, LucideArrowRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+
 import {
   Form,
   FormControl,
